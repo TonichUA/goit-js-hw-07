1ad5e2b38ac580edbbcca8b239e8ee736e462fa1
TonichUA <132302438+TonichUA@users.noreply.github.com>
2023-09-03 22:08:08 +0300

    finish  attempt 3

---
+++ src/js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+function partsGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
+const ul = document.querySelector(".gallery");
+const markup = partsGallery(galleryItems);
+
+ul.insertAdjacentHTML("beforeend", markup);
+ul.addEventListener("click", onClick);
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const original = event.target.dataset.source;
+  const description = event.target.alt;
+
+  // modal
+  const modalInstance = basicLightbox.create(`
+    <div class="modal">
+    <img
+                    class="gallery__image"
+                    src="${original}"
+                    data-source="${original}"
+                    alt="${description}"
+                /> 
+    </div>
+`);
+
+  modalInstance.show();
+
+  document.addEventListener("keydown", onKeyPress);
+}
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+function partsGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+        `;
+    })
+    .join("");
+}
+
+const ul = document.querySelector(".gallery");
+const markup = partsGallery(galleryItems);
+
+ul.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  sourceAttr: "href",
+  captions: true,
+  captionDelay: 250,
+  captionPosition: "bottom",
+  captionType: "attr",
+  captionsData: "alt",
+});
